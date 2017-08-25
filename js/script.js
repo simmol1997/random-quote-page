58364c2a0e59d63ed7cda0a8981d2816b9513aff
simon <simmol@kth.se>
2017-08-25 20:06:44 +0200

    html and style added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+$(document).ready(function() {
 
+  $.getJSON("https://talaikis.com/api/quotes/random/", function(json) {
+
+    let html = '"' + json.quote + '"' + "<br>" + json.author;
+
+    $("#quote-container").html(html);
+  });
+});

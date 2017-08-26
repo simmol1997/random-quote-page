931f4c7f0c46a0ec67d67f591c8ecc5f3dab0566
simon <simmol@kth.se>
2017-08-26 11:54:50 +0200

    all done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
+  generateQuoteAndTweet();
 
-  $.getJSON("https://talaikis.com/api/quotes/random/", function(json) {
-
-    let html = '"' + json.quote + '"' + "<br>" + json.author;
-
-    $("#quote-container").html(html);
+  $("#new-quote-btn").on("click", function() {
+    generateQuoteAndTweet();
   });
 });
+
+function generateQuoteAndTweet() {
+  $.getJSON("https://talaikis.com/api/quotes/random/", function(json) {
+
+    //First I make the quote appear
+    let html = '"' + json.quote + '"' + "<br>" + json.author;
+
+    $("#quote-container").animate({opacity:"0"}, 500);
+    setTimeout(function() {
+
+      $("#quote-container").html(html);
+      
+    }, 500);
+    $("#quote-container").animate({opacity:"1"}, 500);
+
+    //Then we change the tweet-url
+    $("#twitter-share-btn").attr("href", 'https://twitter.com/intent/tweet?text=' + '"' + json.quote + '" ' + json.author);
+  });
+}
